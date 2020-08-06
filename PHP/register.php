8fb6f9a7227c11956aa9adc89ed5363323f8c505
윤주혜 <wngp0805@naver.com>
2020-08-07 01:09:35 +0900

    Update register.php

--- PHP/register.php
+++ PHP/register.php
@@ -4,11 +4,9 @@
 	$userPW = $_POST["userPW"];
 	$gender = $_POST["gender"];
 	$age = $_POST["age"];
-	$skinType = $_POST["skin"];
-	$interest = $_POST["interest"];
 
-	$statement = mysqli_prepare($connect, "INSERT INTO user VALUES (?,?,?,?,?,?)");
-	mysqli_stmt_bind_param($statement, "sssiss", $userID, $userPW, $gender, $age, $skinType, $interest);
+	$statement = mysqli_prepare($connect, "INSERT INTO user VALUES (?,?,?,?)");
+	mysqli_stmt_bind_param($statement, "ssss", $userID, $userPW, $gender, $age);
 	mysqli_stmt_execute($statement);
 
 	$response = array();
