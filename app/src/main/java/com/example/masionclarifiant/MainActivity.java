3e218f5270aea2fc13b1f8a7d295c93ed77ba7b1
saeyeondal <dbtodus1@naver.com>
2020-08-08 22:01:57 +0900

    네비게이션바 추가, 화면 이동 구현

--- app/src/main/java/com/example/masionclarifiant/MainActivity.java
+++ app/src/main/java/com/example/masionclarifiant/MainActivity.java
@@ -1,18 +1,89 @@
 package com.example.masionclarifiant;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    //Variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //메인 화면 어답터설정
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.recently_purchased_products);
+        final ContentMainPageAdapter myPagerAdapter = new ContentMainPageAdapter(getSupportFragmentManager(), 2);
+        viewPager.setAdapter(myPagerAdapter);
+
+        /*--Hooks--*/
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        /*--Navigation Drawer Menu--*/
+
+        //Hide or show items
+        Menu menu = navigationView.getMenu();
+  //      menu.findItem(R.id.nav_logout).setVisible(false);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,  R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        /*--Make a menu clickable--*/
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_home);
     }
 
+    /*-- To avoid closing the application on Back pressed --*/
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                break;
+            case R.id.nav_select:
+                Intent intent2 = new Intent(MainActivity.this, SelectActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.nav_ingredient:
+                break;
+            case R.id.nav_logout:
+                Intent intent4 = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent4);
+            case R.id.nav_mypage:
+                break;
+            case R.id.nav_order:
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
