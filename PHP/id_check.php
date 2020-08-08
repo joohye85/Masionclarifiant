a3d675e60ba4f2f9468f817da95a1675c783e7fd
윤주혜 <wngp0805@naver.com>
2020-08-09 04:34:36 +0900

    아이디 중복검사

---
+++ PHP/id_check.php
@@ -0,0 +1,25 @@
+<?php
+	session_start();
+	$connect = mysqli_connect('maison.c22zsfchgrm1.ap-northeast-2.rds.amazonaws.com:3306','admin','sswu1234','maison');
+	header("Content-Type: application/json;charset=utf-8");
+	$userID = $_POST["userID"];
+
+	
+	$statement = mysqli_prepare($connect, "SELECT * FROM user WHERE userID=?");
+	mysqli_stmt_bind_param($statement, "s", $userID);
+	mysqli_stmt_execute($statement);
+
+	mysqli_stmt_store_result($statement);
+	mysqli_stmt_bind_result($statement, $userID, $userPW, $gender, $age);
+
+	$_SESSION = array(); #세션 배열 생성
+	if(mysqli_stmt_fetch($statement) >= 1){
+		$_SESSION["userID"] = false;
+	}else{
+    	$_SESSION["userID"] = true;
+	}
+
+	
+	$json = json_encode($_SESSION);
+  	echo $json;
+?>
