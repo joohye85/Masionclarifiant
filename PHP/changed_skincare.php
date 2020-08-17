afa7cb0c1d60845f183b1cb4184caded77342642
윤주혜 <wngp0805@naver.com>
2020-08-17 17:28:50 +0900

    Upload 피부타입/집중케어 정보 수정하기

---
+++ PHP/changed_skincare.php
@@ -0,0 +1,35 @@
+<?php
+	session_start();
+	date_default_timezone_set('Asia/Seoul');
+
+	$connect = mysqli_connect('maison.c22zsfchgrm1.ap-northeast-2.rds.amazonaws.com:3306','admin','sswu1234','maison');	
+	$userID = $_POST["userID"];
+	$skinType = $_POST["skinType"];
+	$interest = $_POST["interest"];
+
+	$_SESSION = array();
+	$userID2 = null;
+	
+	$statement = mysqli_prepare($connect, "SELECT userID FROM user WHERE userID =?");
+	mysqli_stmt_bind_param($statement, "s", $userID);
+	mysqli_stmt_execute($statement);
+
+	mysqli_stmt_store_result($statement);
+	mysqli_stmt_bind_result($statement, $userID);
+	while(mysqli_stmt_fetch($statement)){
+		$userID2 = $userID;
+	}
+		if(!empty($userID2)){
+
+			$statement = mysqli_prepare($connect, "UPDATE user SET skinType = '$skinType', interest = '$interest' WHERE userID =?");
+			mysqli_stmt_bind_param($statement, "s", $userID); #skinnum은 자동 증가
+			mysqli_stmt_execute($statement);
+
+			
+			$_SESSION["success"] = true;
+	}else{
+		$_SESSION["success"] = false; #재료 전달 못받음
+	}
+
+	echo json_encode($_SESSION);
+?>
