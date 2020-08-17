c993a4e8eb89217ef31f844a33b0c8e80f80a483
Yoon Joohye <wngp0805@naver.com>
2020-08-17 11:42:33 +0900

    마이페이지 전체 수정 / 화면 구현

---
+++ app/src/main/java/com/example/masionclarifiant/MyskinActivity.java
@@ -0,0 +1,13 @@
+package com.example.masionclarifiant;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyskinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myskin);
+    }
+}
