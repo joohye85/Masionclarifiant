803eed407ac3e4fd897acc9890777b0fd8b789cd
Yoon Joohye <wngp0805@naver.com>
2020-10-20 23:04:25 +0900

    지현+주혜 합침

--- app/src/main/java/com/example/masionclarifiant/DiagnoseResult.java
+++ app/src/main/java/com/example/masionclarifiant/DiagnoseResult.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
@@ -24,6 +27,8 @@ import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.formatter.LargeValueFormatter;
 import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class DiagnoseResult extends AppCompatActivity {
@@ -37,7 +42,11 @@ public class DiagnoseResult extends AppCompatActivity {
     int groupCount = 3;
     public static int moisture = 0; //db에서 수분값 받아옴
     public static int oil = 0; //db에서 유분값 받아옴
-    //public static int blemish = 0; //db에서 잡티 받아옴
+    public static int blemish = 0; //db에서 잡티 받아옴
+    public static double clean = 1; //청결도
+    public static double wrinkle = 2; //주름
+    public static double liver_spot = 3; //기미
+    public static int skin_age = 0; //피부나이
 
 
     @Override
@@ -48,8 +57,24 @@ public class DiagnoseResult extends AppCompatActivity {
         goRecommendBtn = findViewById(R.id.go_recommend_btn);
         moistureBar = (CircularProgressBar)findViewById(R.id.moistureProgressBar);
         oilBar = (CircularProgressBar)findViewById(R.id.oilProgressBar);
+        String userID = getIntent().getStringExtra("userID");
+        Response.Listener<String> res = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+                    JSONObject jsonObject = new JSONObject(response);
 
-        moisture = 33;
+                    clean = jsonObject.getDouble("clean");
+                    liver_spot = jsonObject.getDouble("liver_spot");
+                    wrinkle = jsonObject.getDouble("wrinkle");
+                }catch (Exception e){e.printStackTrace();}
+            }
+        };
+        SkinResultRequest skinResultRequest = new SkinResultRequest(userID, res);
+        RequestQueue queue = Volley.newRequestQueue(DiagnoseResult.this);
+        queue.add(skinResultRequest);
+        System.out.println("테스트으으으으으으"+clean+"ㄴㅁㅇㄴㅇㅁㄴㅇㅁㄴ"+liver_spot+"좀가라제발~!~>!!~"+wrinkle);
+
         moistureBar.setProgress(46); //여기에 수분 수치
         oilBar.setProgress(61); //여기에 유분 수치
 
@@ -65,12 +90,12 @@ public class DiagnoseResult extends AppCompatActivity {
         xVals.add("청결");
         ArrayList yVals1 = new ArrayList();
         ArrayList yVals2 = new ArrayList();
-        yVals1.add(new BarEntry(1, (float) 2));
+        yVals1.add(new BarEntry(1, (float) clean));
         yVals2.add(new BarEntry(1, (float) 1));
-        yVals1.add(new BarEntry(2, (float) 5));
+        yVals1.add(new BarEntry(2, (float) liver_spot));
         yVals2.add(new BarEntry(2, (float) 3));
-        yVals1.add(new BarEntry(3, (float) 6));
-        yVals2.add(new BarEntry(3, (float) 4    ));
+        yVals1.add(new BarEntry(3, (float) wrinkle));
+        yVals2.add(new BarEntry(3, (float) 4));
 
         BarDataSet bar_set1, bar_set2;
         bar_set1 = new BarDataSet(yVals1, "내 피부");
@@ -156,6 +181,23 @@ public class DiagnoseResult extends AppCompatActivity {
         goRecommendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String userID2 = getIntent().getStringExtra("userID");
+                Response.Listener<String> res = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            moisture = jsonObject.getInt("moisture");
+                            oil = jsonObject.getInt("oil");
+                            blemish = jsonObject.getInt("pimple");
+
+                        }catch (Exception e){e.printStackTrace();}
+
+                    }
+                };
+                DiagnoseRequest diagnoseRequest = new DiagnoseRequest(userID2, res);
+                RequestQueue queue = Volley.newRequestQueue(DiagnoseResult.this);
+                queue.add(diagnoseRequest);
                 Intent intent = new Intent(DiagnoseResult.this,DiagnoseRecommend.class);
                 startActivity(intent);
             }
@@ -164,8 +206,10 @@ public class DiagnoseResult extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
+        String userID3 = getIntent().getStringExtra("userID");
         Intent intent = new Intent(DiagnoseResult.this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("userID", userID3);
 
         startActivity(intent);
     }
