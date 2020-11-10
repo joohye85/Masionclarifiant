3f55dc88ee1ab4ae31bda3165916410f5efa81ac
jihyeon <nsa06120@naver.com>
2020-11-10 14:46:22 +0900

    웹뷰만 문제

--- app/src/main/java/com/example/masionclarifiant/SocketService.java
+++ app/src/main/java/com/example/masionclarifiant/SocketService.java
@@ -19,7 +19,7 @@ import java.util.regex.Pattern;
 
 public class SocketService extends Service {
     private Socket socket;
-    public static String wifiModuleIp = "220.69.172.141";
+    public static String wifiModuleIp = "220.69.172.218";
     public static int wifiModulePort = 9999;
     DataOutputStream dataOutputStream;
     DataInputStream dataInputStream;
